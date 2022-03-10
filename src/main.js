d5663caf634a47dee365a5c56f4efd179bfd9311
TAKA <superankar@gmail.com>
2022-03-10 12:58:06 +0900

    色々変更

--- src/main.js
+++ src/main.js
@@ -1,75 +1,40 @@
-//グローバル変数の宣言
-let pomo_remain = 0;
-let pomo_time = 0;
+let SR = document.getElementById("Pomo_start");
+let SP = document.getElementById("Pomo_stop");
 
+//監視
+SP.addEventListener('click',stop());
 //動作部
-let setButton1 = document.getElementById("set_button");
-let startButton = document.getElementById("Pomo_start");
+let pomo = 10;
+let timer_ID;
 
-setButton1.addEventListener("click",onButton);
-startButton.addEventListener("click",Mypomo_start);
-
-//関数ここから
-/*
-関数名:onButton
-動作:
-HTML上のボタン"set-Pomo"が押されたら，設定された値をMyPomo_init(pomo,scale)に引き渡す
-入力:
-*/
-function onButton()
+function start()
 {
-  let pomo;
-  let scale;
-
-  //pomoの設定
-  pomo = document.getElementById("Pomo-select").value;
-  scale = document.getElementById("scale-select").value;
-
-  MyPomo_init(pomo,scale);
+  Mypomo_start();
+  timer_ID = setInterval(Mypomo_start,1000,pomo);
+  SR.disabled = true;
 }
-/*
-関数名:MyPomo_init
-動作:
-入力のpomo,scaleを読み込んで，それぞれのidに対応する値を
-グローバル変数 "pomo_remain","pomo_time"に入れる．
-入力:pomo,scale
-*/
-function MyPomo_init(pomo,scale)
+
+function stop()
 {
-  //ポモの設定
-  if(pomo == "pomo1") pomo_remain = 1;
-  else if(pomo == "pomo2") pomo_remain = 2;
-  else if(pomo == "pomo3") pomo_remain = 3;
-  else if(pomo == "pomo4") pomo_remain = 4;
-  //console.log(pomo_remain);
-
-  //時間の設定
-  if(scale == "15m") pomo_time = 15;
-  else if(scale == "25m") pomo_time = 25;
-  else if(scale == "30m") pomo_time = 30;
-  else if(scale == "45m") pomo_time = 45;
-  else if(scale == "1h") pomo_time = 60;
-  else if(scale == "1.5h") pomo_time = 90;
-
-  pomo_time *= 60;
-
-  /*
-  //デバッグ処理
-  console.log(pomo_remain);
-  console.log(pomo_time);
-  */
+  window.clearInterval(timer_ID);
+  SR.removeAttribute("disabled");
 }
 
 function Mypomo_start()
 {
+  let timer = document.querySelector("#timer");
+  let hour = Math.floor(pomo/3600);
+  let minu = Math.floor(pomo/60);
+  let seco = pomo%60;
 
+  if(Math.floor(hour/10) == 0) hour = '0'+hour;
+  if(Math.floor(minu/10) == 0) minu = '0'+minu;
+  if(Math.floor(seco/10) == 0) seco = '0'+seco;
+
+  timer.textContent = hour + ':' + minu + ':' + seco;
+  if(pomo <= 0)
+  {
+    stop();
+  }
+  pomo--;
 }
-
-
-//temp
-/*
-関数名:
-動作:
-
-入力:
-*/
