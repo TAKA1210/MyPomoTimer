2587002196260d5379721aaf0c200bcf2b08e0b8
TAKA <superankar@gmail.com>
2022-03-14 14:28:34 +0900

    とりあえずタイマー部分を完成

--- src/main2.js
+++ src/main2.js
@@ -1,4 +1,67 @@
-let PPbutton =  document.getElementById("play_pause");
+/*初期化*/
+let pomo = 1500;
+const pomotime = 1500;
+const naptime = 300;
+let start;
+let nap = false;
 
-let test = document.querySelector("#test");
- 
+/*動作部*/
+document.getElementById('timer').style.backgroundColor = '#FFFFF0';
+displaytime(pomotime);
+const PPbutton =  document.getElementById("check");
+PPbutton.checked = false;
+const buttonListen = PPbutton.addEventListener("click",() =>{
+  if(PPbutton.checked)
+  {
+    start = setInterval(checked,1000);
+  }
+  else{
+    clearInterval(start);
+  }
+});
+
+/*関数部*/
+//checkがされている間，タイマーを続ける
+function checked()
+{
+  pomo--;
+  displaytime(pomo);
+  if(pomo <= 0 && PPbutton.checked == true && nap == false)
+  {
+    clearInterval(start);
+    PPbutton.checked = false;
+    pomo = naptime;
+    nap = true;
+    audioPlay();
+    document.getElementById('timer').style.backgroundColor = '#FFFF0F';
+  }
+
+  if(pomo <= 0 && PPbutton.checked == true && nap == true)
+  {
+    clearInterval(start);
+    PPbutton.checked = false;
+    pomo = pomotime;
+    nap = false;
+    audioPlay();
+    document.getElementById('timer').style.backgroundColor = '#FFFFF0';
+  }
+  console.log(nap);
+}
+
+function displaytime(pomo)
+{
+  let timer = document.querySelector("#timer");
+  let hour = Math.floor(pomo/3600);
+  let minu = Math.floor(pomo/60);
+  let seco = pomo%60;
+
+  if(Math.floor(hour/10) == 0) hour = '0'+hour;
+  if(Math.floor(minu/10) == 0) minu = '0'+minu;
+  if(Math.floor(seco/10) == 0) seco = '0'+seco;
+
+  timer.textContent = hour + ':' + minu + ':' + seco;
+}
+function audioPlay()
+{
+
+}
